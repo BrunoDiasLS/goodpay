453c8a8390fc2cdd0106b9ad1996dca3ec7f46f5
BrunoDiasLS <brunodlopes82@globo.com>
2022-09-30 11:11:48 -0300

    iniciando modulo de dados.js

---
+++ js/moduloDados.js
@@ -0,0 +1,39 @@
+/**
+ * Dentro desse array existem usuários com uma estrutura:
+ * id
+ * nomeUsuario
+ * senha
+ * email
+ */
+
+const usuarios = [
+    {
+        id: 1,
+        nomeUsuario: 'Bruno-Dias',
+        senha: '12345',
+        email: 'brunodlopes82@gmail.com',
+    },
+    {
+        id: 2,
+        nomeUsuario: 'jose-almir',
+        senha: '12345',
+        email: 'jose.almir@gmail.com',
+    } ,   {
+        id: 3,
+        nomeUsuario: 'renato3x',
+        senha: '12345',
+        email: 'renato3x@gmail.com',
+    },
+    {
+        id: 4,
+        nomeUsuario: 'Daniel-assuncao',
+        senha: '12345',
+        email: 'daniel.assuncao@gmail.com',
+    }
+] 
+
+export default usuarios;
+
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
